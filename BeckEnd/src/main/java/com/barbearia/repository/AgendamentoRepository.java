2a0ea332a0b8bb4f476643fec4814de3771d1ce4
Douglas Felipe Mariano <159494725+Douglas-Felipe-Mariano@users.noreply.github.com>
2025-11-12 21:39:05 -0300

    BARB 8 - Adicionando Feature de pagamento e adaptando o código para lidar melhor com gerenciamento de acesso pelo tipo de perfil

--- BeckEnd/src/main/java/com/barbearia/repository/AgendamentoRepository.java
+++ BeckEnd/src/main/java/com/barbearia/repository/AgendamentoRepository.java
@@ -20,7 +20,7 @@ import com.barbearia.model.enums.StatusPagamento;
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Integer> {
     Optional<Agendamento> findByBarbeiroAndDataAgendada(Barbeiro barbeiro, LocalDateTime dataAgendada);
 
-    List<Agendamento> findAllByDataAgendada(LocalDateTime dataAgendada);
+    List<Agendamento> findByDataAgendada(LocalDateTime dataAgendada);
     
     // Busca todos os agendamentos de um dia específico (ignora a hora)
     @Query("SELECT a FROM Agendamento a WHERE CAST(a.dataAgendada AS date) = CAST(:data AS date)")
